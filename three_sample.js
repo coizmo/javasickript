5d0eb17327f2a38e3e6145fe606b507cb67e8dca
coizmo <coizmo@gmail.com>
2017-04-28 23:43:48 +0900

    [modify] パラメータの修正

--- three_sample.js
+++ three_sample.js
@@ -4,17 +4,22 @@ var scene = new THREE.Scene();
 //camera
 var fov = 75;
 var aspect = window.innerWidth / window.innerHeight;
-var near = 0.1;
+var near = 1;
 var far = 1000;
+
 var camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-var camera_position_x0 = 0;
-var camera_position_y0 = 0;
-var camera_position_z0 = -10;
 
-camera.position.set(camera_position_x0, camera_position_y0, camera_position_z0);
+camera.position.set(0, 0, 60);
 
+// light
+var light = new THREE.DirectionalLight(0xffffff);
+light.position.set(50, 50, 50);
+scene.add(light);
 
-//renderer
+var ambient = new THREE.AmbientLight(0x333333);
+scene.add(ambient);
+
+// renderer
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
@@ -27,11 +32,20 @@ var orbitControls = new THREE.OrbitControls(camera)
  * draw objects
  */
 
-var sphereGeometry = new THREE.SphereGeometry( 50, 36, 36 );
+var sphereGeometry = new THREE.SphereGeometry(40, 16, 16);
+sphereGeometry.scale(-1, 1, 1);
 var sphereMaterial = new THREE.MeshBasicMaterial( {color: 0x0080e3, wireframe: true } );
+var sphereMaterialTexture = new THREE.MeshPhongMaterial({
+	color: 0xffffff,
+	ambient: 0xffffff,
+	specular: 0xcccccc,
+	shininess: 50,
+	metal: true,
+	map: new THREE.TextureLoader('./image.jpg')
+});
 var sphere = new THREE.Mesh( sphereGeometry, sphereMaterial );
 sphere.position.set( 0, 0, 0 );
-scene.add( sphere );
+scene.add(sphere);
 
 
 function rendererRender() {
